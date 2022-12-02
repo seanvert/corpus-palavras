38bf05af57c30d23370636e1baf7d11a9ee9c68b
Sean Uchida <seankuchida@gmail.com>
2022-12-02 15:27:37 -0300

    reading file and parsing properly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use tl::*;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("testedados")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let dom = tl::parse(&mut contents, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+    let element = dom
+        .get_element_by_id("8")
+        .expect("Failed to find element")
+        .get(parser)
+        .unwrap();
+    println!("{:?}", element);
+
+    Ok(())
 }
