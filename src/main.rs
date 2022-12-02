7cb4ab91e156872334bb7f6fd11f013f5d283f03
Sean Uchida <seankuchida@gmail.com>
2022-12-02 20:06:16 -0300

    counting words and saving them in a hash

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
+use core::option::Option;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
-use tl::*;
-
+use std::string::ToString;
+use tl::{parse, Bytes, ParserOptions};
 fn main() -> std::io::Result<()> {
     let mut file = File::open("testedados")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let dom = tl::parse(&mut contents, tl::ParserOptions::default()).unwrap();
     let parser = dom.parser();
-    let element = dom
-        .get_element_by_id("8")
-        .expect("Failed to find element")
-        .get(parser)
-        .unwrap();
-    println!("{:?}", element);
 
+    let mut map: HashMap<String, i32> = HashMap::with_capacity(10000);
+    // separar os fragmentos
+    for n in 1..10 {
+        let string = String::from(n.to_string()).into_bytes();
+        let mut input = tl::Bytes::new();
+        input.set(string);
+        let element = dom
+            .get_element_by_id(input)
+            .expect("Failed to find element")
+            .get(parser)
+            .unwrap();
+        // separar o texto
+        let text = element.inner_text(parser);
+        // TODO remover caracteres especiais
+        let words = text.split(" ");
+        // separar as palavras
+        for token in words {
+            let key = String::from(token);
+            // inserir as palavras no hash
+            if map.contains_key(&key) {
+                *map.get_mut(&key).unwrap() += 1;
+            } else {
+                map.insert(key, 1);
+            };
+
+            // contar as palavras
+        }
+
+        // salvar arquivo
+    }
+    // if None::<i32> == None {
+    //     println!("asd");
+    // }
+    for (key, value) in map {
+        println!("{key}, {value}");
+    }
     Ok(())
 }
